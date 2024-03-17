de2a5256a050ca4dcf062924f9179c4b7ca459bd
Belendia Serda <belendia@gmail.com>
2024-03-17 08:46:56 +0300

    Add button sharing code

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import Button from 'remoteApp/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +10,7 @@ function App() {
     <>
 
       <h1>Host Application</h1>
+      <Button />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
